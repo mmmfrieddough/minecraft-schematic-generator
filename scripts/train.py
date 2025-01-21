097cdb537548f3c5bf994f29e85cf8c25d70b61d
Patrizio Spagnardi III <mmmfrieddough@gmail.com>
2025-01-20 23:04:21 -0500

    Various changes

--- scripts/train.py
+++ scripts/train.py
@@ -16,7 +16,7 @@ def main():
     torch.set_float32_matmul_precision("medium")
 
     experiment_name = "mini_model"
-    experiment_version = 8
+    experiment_version = 20
     checkpoint_dir = "lightning_logs"
     tensorboard_logger = TensorBoardLogger(
         checkpoint_dir, name=experiment_name, version=experiment_version
@@ -36,36 +36,32 @@ def main():
         num_layers=2,
         decoder_dropout=0.1,
         max_learning_rate=1e-4,
-        warmup_steps=20000,
     )
 
-    hdf5_file = "data/data_v2.h5"
-    batch_size = 50
     data_module = MinecraftDataModule(
-        file_path=hdf5_file,
-        batch_size=batch_size,
+        file_path="data/data_v2.h5",
+        batch_size=50,
+        num_workers=2,
         combine_datasets=True,
         separate_validation_datasets=["holdout"],
     )
 
     latest_checkpoint_callback = ModelCheckpoint(save_last=True)
     best_model_checkpoint_callback = ModelCheckpoint(
-        save_top_k=3, monitor="val_loss", mode="min"
+        save_top_k=2, monitor="val_loss", mode="min"
     )
     lr_monitor_callback = LearningRateMonitor()
     block_benchmark_callback = BlockBenchmarkCallback(num_runs=200)
 
-    ddp = DDPStrategy(process_group_backend="gloo")
+    ddp = DDPStrategy(process_group_backend="gloo", find_unused_parameters=False)
 
     trainer = Trainer(
         strategy=ddp,
-        devices=2,
-        max_epochs=5000,
+        max_epochs=1,
         logger=[tensorboard_logger, wandb_logger],
-        gradient_clip_val=1.0,
-        log_every_n_steps=50,
-        val_check_interval=5000,
-        limit_val_batches=1000,
+        val_check_interval=0.1,
+        limit_val_batches=0.2,
+        accumulate_grad_batches=4,
         callbacks=[
             latest_checkpoint_callback,
             best_model_checkpoint_callback,
