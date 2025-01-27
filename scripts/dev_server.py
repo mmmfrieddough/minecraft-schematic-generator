a2b23093c48e8c325e891006464444fea1fbeaeb
Patrizio Spagnardi III <mmmfrieddough@gmail.com>
2025-01-27 18:55:12 -0500

    Switch to Hypercorn for server Improve logging

--- scripts/dev_server.py
+++ scripts/dev_server.py
@@ -1,12 +1,27 @@
-import uvicorn
+import asyncio
+import logging
+
+from hypercorn.asyncio import serve
+from hypercorn.config import Config
 
 from minecraft_schematic_generator.server import app
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(process)d] [%(levelname)s] %(message)s",
+    datefmt="[%Y-%m-%d %H:%M:%S %z]",
+)
 
 if __name__ == "__main__":
     app.state.mode = "local"
     app.state.checkpoint_path = (
-        "lightning_logs/mini_model/version_8/checkpoints/last.ckpt"
+        # "lightning_logs/mini_model/higher_max_lr/checkpoints/last.ckpt"
+        "data/center_data/version_12/checkpoints/last.ckpt"
     )
 
-    print("Starting server")
-    uvicorn.run(app, host="0.0.0.0", port=8000, timeout_keep_alive=5)
+    config = Config()
+    config.bind = ["127.0.0.1:8000"]
+    config.accesslog = logging.getLogger("hypercorn.access")
+    config.errorlog = logging.getLogger("hypercorn.error")
+
+    asyncio.run(serve(app, config))
